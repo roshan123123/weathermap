6cac492d1dce53715d5aa22fc11ed3560338fe61
Roshan <kumarroshan7461@gmail.com>
2021-04-08 18:17:02 +0530

    second testing

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post("/",function (req,res)//to respond to req from form
      // const stringifydata=JSON.stringify(data);//stringify
      // console.log(stringifydata);
      // console.log(parsedata.weather[0].id);//accesssing parsed data
-     const temp=parsedata.main.temp;
+     const temp=parsedata.main.temp;//right temp  is for the temoerature
      res.write("<h1>The temperature in  " +city+ " is "+temp+" K</h1>");//we can have many res .write and pass html elements in string
                           //only h1 works in res.write and p an
      res.send();//we can have only one res.send in a get
@@ -42,6 +42,36 @@ app.post("/",function (req,res)//to respond to req from form
 
 
 });
+
+
+
+// app.post("/",function (req,res)//to respond to req from form
+// {
+//
+//
+//   const city=req.body.city;
+//   const aid="e37b2d7b13209bcbbfca6cf86ce5da8c";
+//     const url ="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+aid
+//      https.get(url,function(response)
+//    {
+//      console.log(response.statusCode);
+//
+//      response.on("data",function(data)
+//    {
+//      const parsedata=JSON.parse(data);//parsing
+//      // console.log(parsedata);
+//      // const stringifydata=JSON.stringify(data);//stringify
+//      // console.log(stringifydata);
+//      // console.log(parsedata.weather[0].id);//accesssing parsed data
+//      const temp=parsedata.main.temp;//right temp  is for the temoerature
+//      res.write("<h1>The temperature in  " +city+ " is "+temp+" K</h1>");//we can have many res .write and pass html elements in string
+//                           //only h1 works in res.write and p an
+//      res.send();//we can have only one res.send in a get
+//    });
+//  });//works without ; also
+//
+//
+// });
 app.listen(process.env.PORT|| 3000,function()
 {
   console.log("server working");
